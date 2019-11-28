be9ee6a09f97c4926682144e10808856543b5d3b
likevintw <40978103+likevintw@users.noreply.github.com>
2019-11-28 17:04:33 +0800

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,42 @@
+﻿
+#include "Radix2FixedPointFFT.h"
+
+void Fixed_FFT_2radix_Example()
+{
+	Fixed_FFT_2radix FFTtest;
+	// Set data size
+	FFTtest.data_size = 128; // The Number must be equal or smaller than 128.
+
+	// Initializtion
+	FFTtest.FFT_2radix_initialization(); 
+	// Set Input Data
+	printf("Input Data\n");
+	for (int i(0); i < FFTtest.data_size; i++)
+	{
+		FFTtest.re[i] = 30000 * sin(i * 30 / 57.3);
+		FFTtest.im[i] = 0; //All of Image parts must be set as 0
+		printf("%d\n", FFTtest.re[i]); //Show the Input Data
+	}
+
+	// FFT Algorithm
+	FFTtest.FFT_2radix_algorithm(); 
+
+	// Show Result
+	printf("Show Fixed FFT Result\n");
+	for (int i(0); i < FFTtest.data_size >> 1; i++)
+	{
+		//printf("%d %d \n", FFTtest.re[i], FFTtest.im[i]); //Show the result
+		printf("%d\n", FFTtest.output_data_Amplitude[i]);
+	}
+
+}
+
+
+
+
+int main()
+{
+	printf("Fixed Point FFT Example\n");
+	Fixed_FFT_2radix_Example();
+	return 0;
+}
---
+++ Radix2FixedPointFFT.h
@@ -0,0 +1,22 @@
+﻿//Radix2 Fixed Point FFT A7
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+//Fixed_FFT_2radix
+/**/
+#define PI 3.14159
+#define angle2radian 57.29
+class Fixed_FFT_2radix
+{
+public:
+	int* re, * im;
+	int data_size;
+	int* cost, * sint;
+	int* output_data_Amplitude;
+	//
+	Fixed_FFT_2radix();
+	void FFT_2radix_initialization();
+	void FFT_2radix_algorithm();
+};
